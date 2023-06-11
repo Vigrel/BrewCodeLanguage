d4ebf53d9622f4a239166c4f6a3ba3fbafa13377
Vigrel <eller.vini@gmail.com>
2023-06-11 14:47:55 -0300

    add symbolTable to evaluate

--- main.py
+++ main.py
@@ -4,19 +4,7 @@ from CoffeeParser import CoffeeParser
 
 text_input = """
 Let's brew a coffee
-  cup blackCoffee = 10;
-  cup americanCoffee = 0;
-  cup water = 1;
-  percolate(blackCoffee stronger than americanCoffee) {
-    sip (blackCoffee weaker than 5) {
-      sip(blackCoffee weaker than 2) {
-        serve "No sugar added";
-      } gulp {
-        serve "Adding sugar";
-      }
-    }
-    blackCoffee = blackCoffee - water;
-  }
+
 Your coffee is ready
 """
 lexer = CoffeeLexer().get_lexer()
@@ -25,4 +13,4 @@ tokens = lexer.lex(text_input)
 pg = CoffeeParser()
 pg.parse()
 parser = pg.get_parser()
-parser.parse(tokens).evaluate()
+parser.parse(tokens).evaluate(pg.st)
